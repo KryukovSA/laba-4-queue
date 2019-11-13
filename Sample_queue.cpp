acebf4fbeba7e825cef839d1c9f8bc1010bb0c68
Kryukov.SA <45076012+KryukovSA@users.noreply.github.com>
2019-11-13 14:43:51 +0300

    Add files via upload

---
+++ Sample_queue.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "TProc.h"
+using namespace std;
+
+int main()
+{
+
+
+
+
+
+
+
+system("pause");
+	return 0;
+	
+}
---
+++ TProc.h
@@ -0,0 +1,5 @@
+#pragma once
+#include"queue.h"
+#include<iostream>
+#include <Windows.h>
+#include <vector>
